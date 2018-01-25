656e16b4e9e3239afd161435c8af2eb0f630a8e5
hugojuradodsindigo <hugo@dsindigo.mx>
2018-01-25 16:57:17 -0600

    Refactorizacion valiacion fizzbuzz fix
    Se refactorizo las validaciones de fizz, buzz y fizzbuzz en metodos diferentes

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -3,14 +3,26 @@ package fizzbuzz;
 public class FizzBuzz {
     
     public String evaluteFizzBuzzNumber(int number){
-        if (number % 3 == 0 && number % 5 == 0) {
+        if (isFizzBuzz(number)) {
             return "fizzbuzz";
-        }else if (number % 3 == 0) {
+        }else if (isFizz(number)) {
             return "fizz";
-        }else if (number % 5 == 0) {
+        }else if (isBuzz(number)) {
             return "buzz";
         }else {
             return String.valueOf(number);
         }
+    }
+    
+    private boolean isFizz(int number) {
+        return number % 3 == 0;
+    }
+    
+    private boolean isBuzz(int number) {
+        return number % 5 == 0;
+    }
+    
+    private boolean isFizzBuzz(int number) {
+        return (number % 3 == 0 && number % 5 == 0);
     }
 }

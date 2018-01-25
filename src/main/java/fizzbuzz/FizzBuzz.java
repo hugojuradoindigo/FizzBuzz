8c66cca325498708b4fdd2f49b90cbe4cecefdef
hugojuradodsindigo <hugo@dsindigo.mx>
2018-01-25 16:36:39 -0600

    Validacion multiples de 3 resolve
    Se implemento una validacion para los multiples de 3

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,10 @@ package fizzbuzz;
 public class FizzBuzz {
     
     public String evaluteFizzBuzzNumber(int number){
+        if (number % 3 == 0) {
+            return "fizz";
+        }else {
             return String.valueOf(number);
+        }
     }
 }

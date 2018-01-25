f171d4afa28a0134911184cde21c2c9bf2de0e40
hugojuradodsindigo <hugo@dsindigo.mx>
2018-01-25 16:31:03 -0600

    Prueba no multiples de 3 y 5 fix
    Se implemento la prueba para regresar valor numerico

--- src/test/java/fizzbuzz/TestFizzBuzz.java
+++ src/test/java/fizzbuzz/TestFizzBuzz.java
@@ -4,4 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestFizzBuzz {
+
+	FizzBuzz fizzBuzz = new FizzBuzz();
+	
+	@Test
+	public void testNumber() {
+		Assert.assertEquals(fizzBuzz.evaluteFizzBuzzNumber(1), "1");
+	}
 }

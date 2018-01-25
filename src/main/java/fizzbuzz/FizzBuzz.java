34f4ffe23a6e4082c2800c107a69cf71ad7ee254
hugojuradodsindigo <hugo@dsindigo.mx>
2018-01-25 16:43:42 -0600

    Validacion buzz resolve
    Se implemento una validacion para buzz en dado caso que sea multiple de 5

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     public String evaluteFizzBuzzNumber(int number){
         if (number % 3 == 0) {
             return "fizz";
+        }else if (number % 5 == 0) {
+            return "buzz";
         }else {
             return String.valueOf(number);
         }
